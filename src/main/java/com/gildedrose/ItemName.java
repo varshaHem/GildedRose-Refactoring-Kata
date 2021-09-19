9ce6c301c1c473f8095df0b8b996330a066a1aa3
928280 <Varsha.Hemnani@Cognizant.com>
2021-09-19 21:41:07 +0200

    add gildedrose requirement

--- src/main/java/com/gildedrose/ItemName.java
+++ src/main/java/com/gildedrose/ItemName.java
@@ -4,7 +4,8 @@ public enum ItemName  {
 
     AGED_BRIE("Aged Brie"),
     BACKSTAGE_PASSES("Backstage passes to a TAFKAL80ETC concert"),
-    SULFURAS("Sulfuras, Hand of Ragnaros");
+    SULFURAS("Sulfuras, Hand of Ragnaros"),
+    CONJURED("Conjured");
 
     private final String itemName;
 
